4f289412ae082e670129bab2e29601ef511262a5
earthddx <47094239+earthddx@users.noreply.github.com>
2020-01-01 13:41:20 -0500

    change

---
+++ build/precache-manifest.43d1611d419a054fa81b942c2b2e42b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dfe5b00e89c6a402408b9488c089ff",
+    "url": "/wordCountGame/index.html"
+  },
+  {
+    "revision": "eee8adea2a03c63fa62a",
+    "url": "/wordCountGame/static/css/main.6f7c88cd.chunk.css"
+  },
+  {
+    "revision": "e721283d6406d1efa3c1",
+    "url": "/wordCountGame/static/js/2.f438b240.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wordCountGame/static/js/2.f438b240.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eee8adea2a03c63fa62a",
+    "url": "/wordCountGame/static/js/main.cce1f557.chunk.js"
+  },
+  {
+    "revision": "733f4ee974cad555d562",
+    "url": "/wordCountGame/static/js/runtime-main.d014e900.js"
+  }
+]);
